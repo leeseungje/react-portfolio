a4a284c65a1dd70d4fc72ba191b88a41f1c9e4a4
Lee Seungje <leeseungje@gamevilcom2us.com>
2020-08-27 11:41:15 +0900

    수정

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.45122319dd75c2e6c48437d0528b760a.js"
+  "/react-portfolio/precache-manifest.d8cd8dea8e1daa991abdee459c76c4e8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
